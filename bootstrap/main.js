ee90ae48df626a6135f8b72ad221a47bd6d414b4
FranciscoBa2 <70809546+FranciscoBa2@users.noreply.github.com>
2021-09-14 16:13:13 -0300

    Update main.js

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -1,14 +1,15 @@
 var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-    }
-    ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight
-    + "px";
-    })
-    
-setTimeout(function() {
-    $('#demo-modal').modal();
-    }, 500);
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
 
-    
+ready(() => { 
+	document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+// set modal time delay before loading
+
+setTimeout(function() {
+	$('#demo-modal').modal();
+}, 500);
+
